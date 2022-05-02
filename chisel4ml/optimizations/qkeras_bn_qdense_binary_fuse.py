7b7b4cf1b0c8523e5b1562e4cb3704b852519177
jvreca <jure.vreca@ijs.si>
2022-05-02 22:07:08 +0200

    Implemented the batchnorm fusing. Not yet tested.

--- chisel4ml/optimizations/qkeras_bn_qdense_binary_fuse.py
+++ chisel4ml/optimizations/qkeras_bn_qdense_binary_fuse.py
@@ -2,7 +2,12 @@ import qkeras
 
 from tensorflow.keras.layers import Layer as KerasLayer
 from tensorflow.keras.layers import BatchNormalization
+from tensorflow.math import sqrt
+from tensorflow.math import multiply as mul
+from tensorflow.math import truediv as div
+
 from typing import List
+import copy
 
 from chisel4ml.optimizations.qkeras_optimization import QKerasOptimization
 from chisel4ml.optimizations import register_qkeras_optimization
@@ -13,15 +18,15 @@ class QKerasBnQdenseBinaryFuse(QKerasOptimization):
     num_layers = 3
 
     def __call__(self, layers: List[KerasLayer]) -> List[KerasLayer]:
-        if self.is_applicable(layers):
-            # moving_mean = layers[1].moving_mean
-            # moving_variance = layers[1].moving_variance
-            # beta = layers[1].beta
-            # gamma = layers[1].gamma
-            # bias = layers[0].bias
-            return layers
-        else:
-            return layers
+        new_layers = copy.deepcopy(layers)
+        mm = layers[1].moving_mean
+        mv = layers[1].moving_variance
+        b = layers[1].beta
+        g = layers[1].gamma
+        b = layers[0].bias
+        new_layers[0].bias = (mm - b) - div(mul(sqrt(mv), b), g)
+        del new_layers[1]
+        return new_layers
 
     def is_applicable(self, layers: List[KerasLayer]) -> bool:
         return (type(layers[0]) is qkeras.QDense and
