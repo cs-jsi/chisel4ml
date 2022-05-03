b2bc364af2e132ca600354a1b523a26d052624a9
jvreca <jure.vreca@ijs.si>
2022-05-03 10:09:01 +0200

    Added new sliding_window iterator that sort of solves the optimization application problem.

--- chisel4ml/optimize.py
+++ chisel4ml/optimize.py
@@ -3,26 +3,35 @@ from chisel4ml.optimizations import qkeras_opt_list
 import tensorflow as tf
 
 import copy
+import collections
+import itertools
 
-MAX_PASSES = 3
+NUM_ITERATIONS = 3  # Some optimizations can only be applied after certain others optimizations have been applied.
 
 
 def qkeras_model(model):
     "Applys optimization passes to the model, and returns a dummy model that can be transformed into a LBIR model."
     layers = copy.deepcopy(model.layers)  # layers in keras are read-only
 
-    def _rolling_window(list, degree):
-        for i in range(len(list)-degree+1):
-            yield [list[i+o] for o in range(degree)]
+    def sliding_window(iterable, size):
+        iterable = iter(iterable)
+        window = collections.deque(
+            itertools.islice(iterable, size-1),
+            maxlen=size
+        )
+
+        for index, item in enumerate(iterable):
+            window.append(item)
+            yield tuple((index, window))
+
 
     # Some layers are wrapped in other layers (pruning layer i.e.) in the first pass we unwrapp it and then
     # we apply other optimizations.
-    for _ in range(MAX_PASSES):
+    for _ in range(NUM_ITERATIONS):
         for opt in qkeras_opt_list:
-            for lslice in _rolling_window(layers, opt.num_layers):
-                assert len(lslice) > 0
+            for i, lslice in sliding_window(layers, opt.num_layers):
                 if opt.is_applicable(lslice):
-                    lslice = opt(lslice)  # TODO: is this safe?
+                    layers[i:i+opt.num_layers] = opt(lslice)  # TODO: is this safe?
 
     # Re-create the model
     new_model = tf.keras.models.Sequential()
