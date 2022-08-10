d5ac52f15910f3ae95de738a461d79164676e161
jvreca <jure.vreca@ijs.si>
2022-08-10 09:51:04 +0200

    Added transform test for non binarized nets.

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -57,6 +57,7 @@ def test_qkeras_transform(bnn_mnist_model):
 
     # input
     assert layers[0].input.dtype.quantization is UNIFORM, "The first layers input is uniformly quantized."
+    assert int(layers[0].input.dtype.bitwidth) == 8
     for lay in layers[1:]:
         assert lay.input.dtype.quantization is BINARY, \
                 "Except for the first layer these should all have binary input quantization."
@@ -70,6 +71,69 @@ def test_qkeras_transform(bnn_mnist_model):
     for lay in layers:
         assert lay.activation.fn is BINARY_SIGN
         assert int(lay.activation.bitwidth) == 1
+
+    # out_shape
+    for lay in layers:
+        assert len(lay.output.shape) > 0
+
+
+def test_qkeras_transform_2(sint_mnist_qdense_relu):
+    opt_model = optimize.qkeras_model(sint_mnist_qdense_relu)
+    lbir_model = transform.qkeras_to_lbir(opt_model)
+    layers = lbir_model.layers
+
+    UNIFORM = lbir.Datatype.QuantizationType.UNIFORM
+
+    # ltype
+    for lay in layers:
+        assert lay.ltype is lbir.Layer.DENSE, "There should be only DENSE layers left here."
+
+    # use_bias
+    for lay in layers:
+        assert lay.use_bias is True
+
+    # biases
+    assert layers[0].biases.dtype.quantization is UNIFORM
+    assert int(layers[0].biases.dtype.bitwidth) == 32
+    assert int(layers[0].biases.dtype.scale) == 1
+    assert int(layers[0].biases.dtype.offset) == 0
+    assert len(layers[0].biases.shape) > 0
+    assert len(layers[0].biases.values) > 0
+    for lay in layers[1:-1]:
+        assert lay.biases.dtype.quantization is UNIFORM
+        assert int(lay.biases.dtype.bitwidth) == 32
+        assert int(lay.biases.dtype.scale) == 1
+        assert int(lay.biases.dtype.offset) == 0
+        assert len(lay.biases.shape) > 0
+        assert len(lay.biases.values) > 0
+
+    assert layers[-1].biases.dtype.quantization is UNIFORM
+    assert int(layers[-1].biases.dtype.bitwidth) == 32
+    assert int(layers[-1].biases.dtype.scale) == 1
+    assert int(layers[-1].biases.dtype.offset) == 0
+    assert len(layers[-1].biases.shape) > 0
+    assert len(layers[-1].biases.values) > 0
+
+    # weights
+    for lay in layers:
+        assert lay.weights.dtype.quantization is UNIFORM
+        assert int(lay.weights.dtype.bitwidth) == 4
+        assert int(lay.weights.dtype.scale) == 1
+        assert int(lay.weights.dtype.offset) == 0
+        assert len(lay.weights.shape) > 0
+        assert len(lay.weights.values) > 0
+
+    # input
+    assert layers[0].input.dtype.quantization is UNIFORM
+    assert int(layers[0].input.dtype.bitwidth) == 8
+    for lay in layers[1:]:
+        assert lay.input.dtype.quantization is UNIFORM, \
+                "All layers should have a uniform input quantization."
+        assert int(lay.input.dtype.bitwidth) == 4
+        assert int(lay.input.dtype.scale) == 1
+        assert int(lay.input.dtype.offset) == 0
+        assert len(lay.input.shape) > 0
+        assert len(lay.input.values) == 0  # Input values should be empty.
 
     # out_shape
     for lay in layers:
