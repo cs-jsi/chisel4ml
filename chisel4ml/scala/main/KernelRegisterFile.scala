cca8c4531e13c60fafb30dcdc8c6ecb6420f68c5
jvreca <jure.vreca@ijs.si>
2023-01-03 12:13:59 +0100

    Basic test of KernelRegisterFile working.

---
+++ chisel4ml/scala/main/KernelRegisterFile.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022 Computer Systems Department, Jozef Stefan Insitute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package chisel4ml.sequential
+
+import chisel3._
+import chisel3.util._
+
+import _root_.chisel4ml.util.log2
+import _root_.chisel4ml.implicits._
+
+/** A register file for storing the weights/kernel of a convolution layer.
+  *
+  * kernelSize: Int - Signifies one dimension of a square kernel (If its a 3x3 kernel then kernelSize=3) kernelDepth:
+  * kernelDepth: Int - The depth of the kernel (number of input channels) actParamSize: Int - The activation
+  * parameterSize in bits. kernelParamSize: Int - Bitwidth of each kernel parameter.
+  */
+class KernelRegisterFile(kernelSize: Int, kernelDepth: Int, kernelParamSize: Int) extends Module {
+    val totalNumOfElements:  Int = kernelSize * kernelSize * kernelDepth
+    val kernelNumOfElements: Int = kernelSize * kernelSize
+    val wrDataWidth:         Int = kernelNumOfElements * kernelParamSize
+    val outDataSize:         Int = kernelSize * kernelSize * kernelDepth * kernelParamSize
+    val kernelAddrWidth:     Int = math.floor(log2(kernelDepth.toFloat)).toInt + 1
+    val rowAddrWidth:        Int = math.ceil(log2(kernelSize.toFloat)).toInt
+
+    val io = IO(new Bundle {
+        val kernelAddr = Input(UInt(kernelAddrWidth.W))
+        val inData     = Input(UInt(wrDataWidth.W))
+        val inValid    = Input(Bool())
+        val outData    = Output(UInt(outDataSize.W))
+    })
+
+    val regs = RegInit(VecInit.fill(kernelDepth, kernelNumOfElements)(0.U(kernelParamSize.W)))
+
+    when(io.inValid) {
+        regs(io.kernelAddr) := io.inData.asTypeOf(Vec(kernelNumOfElements, UInt(kernelParamSize.W)))
+    }
+
+    io.outData := regs.asUInt
+}
---
+++ chisel4ml/scala/test/KernelRegisterFileTests.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 Computer Systems Department, Jozef Stefan Insitute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package chisel4ml.tests
+
+import org.scalatest.flatspec.AnyFlatSpec
+import chisel3._
+import chisel3.experimental.VecLiterals._
+import chiseltest._
+
+import _root_.chisel4ml.sequential._
+import _root_.chisel4ml.implicits._
+
+import _root_.org.slf4j.Logger
+import _root_.org.slf4j.LoggerFactory
+
+class KernelRegisterFileTests extends AnyFlatSpec with ChiselScalatestTester {
+    val logger = LoggerFactory.getLogger(classOf[KernelRegisterFileTests])
+
+    behavior.of("KernelRegisterFile module")
+    it should "show appropirate window as it cycles through the input image" in {
+        test(new KernelRegisterFile(2, 2, 4)) { dut =>
+            dut.io.kernelAddr.poke(0.U)
+            dut.io.inData.poke("b0011_0010_0001_0000".U)
+            dut.io.inValid.poke(true.B)
+            dut.clock.step()
+            dut.io.kernelAddr.poke(1.U)
+            dut.io.inData.poke("b0111_0110_0101_0100".U)
+            dut.clock.step()
+            dut.io.inValid.poke(false.B)
+            dut.io.outData.expect("b0111_0110_0101_0100_0011_0010_0001_0000".U)
+            dut.clock.step()
+        }
+    }
+}
